e9988612f32342f2e8304d9199786cbca3aced09
GabrielKKKJJ <djordan.bezerra@jala.university>
2024-10-09 14:50:58 -0300

    finalização do site

---
+++ scripts/home.js
@@ -0,0 +1,9 @@
+function showMenCloths() {
+  document.getElementById("women-cloths").style.display = "none";
+  document.getElementById("men-cloths").style.display = "block";
+}
+
+function showWomenCloths() {
+  document.getElementById("men-cloths").style.display = "none";
+  document.getElementById("women-cloths").style.display = "block";
+}
